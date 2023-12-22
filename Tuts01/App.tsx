2e8c20c5d7ef9972c43d6d60d3bf2f89dfa3a898
raipratik <pratikkumar399@gmail.com>
2023-12-22 17:53:30 +0530

    first commit

---
+++ Tuts01/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Himanshi Bhardwaj Rai</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
