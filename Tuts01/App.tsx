b85c013546a9d3c4937d8f98f094d2227114cf66
raipratik <pratikkumar399@gmail.com>
2023-12-25 01:06:10 +0530

    structuring the multi-file components

--- Tuts01/App.tsx
+++ Tuts01/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
+import FlatCards from './components/FlatCards';
 
 function App() {
   return (
     <SafeAreaView>
       <View>
         <Text>Himanshi Bhardwaj Rai</Text>
+        <FlatCards />
       </View>
     </SafeAreaView>
   );
